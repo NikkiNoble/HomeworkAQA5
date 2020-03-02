68b81fdf0541ae1761041d414a40499f0d2cc3b4
Anastasia Motolova <motolovanastya@gmail.com>
2020-03-02 11:40:22 -0500

    Feature added

--- src/test/java/ru/netology/ApplicationCardDeliveryTest.java
+++ src/test/java/ru/netology/ApplicationCardDeliveryTest.java
@@ -1,59 +1,54 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.Keys.BACK_SPACE;
+import ru.netology.data.CardPage;
 
 public class ApplicationCardDeliveryTest {
-
-    private SelenideElement date = $("[data-test-id=date] input");
-    private String page = "http://localhost:9999";
-    private SelenideElement agreement = $("[data-test-id=agreement]");
-    private SelenideElement cityName = $("[data-test-id=city] input");
-    private SelenideElement personName = $("[data-test-id=name] input");
-    private SelenideElement phoneNumber = $("[data-test-id=phone] input");
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
 
     @Test
-    public void ShouldSubmitRequestAndChangeBookedDate(){
-        open("http://localhost:9999");
-        cityName.setValue(DataGenerator.generateNewApplication().getCity());
-        personName.setValue(DataGenerator.generateNewApplication().getName());
-        phoneNumber.setValue(DataGenerator.generateNewApplication().getPhone());
-        agreement.click();
-        $$("button").find(exactText("Запланировать")).click();
-        $(withText("Успешно")).waitUntil(visible, 5000);
-        date.doubleClick().sendKeys(BACK_SPACE);
-        date.setValue(DataGenerator.generateNewApplication().getDate());
-        $$("button").find(exactText("Запланировать")).click();
-        $$("button").find(exactText("Перепланировать")).click();
-        $(withText("Успешно")).waitUntil(visible, 5000);
+    void shouldSubmitRequestAndChangeBookedDate() {
+        CardPage cardApplication = new CardPage();
+        cardApplication.openPage();
+        cardApplication.sendRequestWithAllFieldsFilledIn();
+        cardApplication.findSuccessElement();
+        cardApplication.replanWithNewDate();
     }
 
     @Test
-    void ShouldNotSubmitRequest(){
-        open("http://localhost:9999");
-        personName.setValue(DataGenerator.generateNewApplication().getName());
-        phoneNumber.setValue(DataGenerator.generateNewApplication().getPhone());
-        agreement.click();
-        $$("button").find(exactText("Запланировать")).click();
-        $(withText("Поле обязательно для заполнения")).waitUntil(visible, 5000);
+    void shouldNotSubmitRequest(){
+        CardPage cardApplication = new CardPage();
+        cardApplication.openPage();
+        cardApplication.sendRequestWithEmptyFields();
+        cardApplication.findError();
     }
 
     @Test
-    void ShouldNotSubmitRequestWithWrongData(){
-        open("http://localhost:9999");
-        cityName.setValue(DataGenerator.generateNewErrorApplication().getCity());
-        personName.setValue(DataGenerator.generateNewErrorApplication().getName());
-        phoneNumber.setValue(DataGenerator.generateNewErrorApplication().getPhone());
-        agreement.click();
-        $$("button").find(exactText("Запланировать")).click();
-        $(withText("Доставка в выбранный город недоступна")).waitUntil(visible, 5000);
+    void shouldNotSubmitRequestWithWrongData(){
+        CardPage cardApplication = new CardPage();
+        cardApplication.openPage();
+        cardApplication.sendRequestWithWrongData();
+        cardApplication.findErrorElement();
+    }
+
+    @Test
+    void shouldSubmitRequest() {
+        CardPage cardApplication = new CardPage();
+        cardApplication.openPage();
+        cardApplication.sendRequestWithSpecificName();
+        cardApplication.findSuccessElement();
     }
 
 }
